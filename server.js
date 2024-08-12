3b17ee634edd8b3a602e049b2ffa2378171211eb
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-08-12 21:37:51 +0900

    feat: Initial Setup

---
+++ server.js
@@ -0,0 +1,34 @@
+console.log("web serverni boshlash");
+
+const express = require("express");
+const app = express();
+const https = require("http");
+
+//1  Kirish
+app.use(express.static("public"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+//2 Session
+//3  Views
+app.set("views", "view");
+app.set("view engine", "ejs");
+
+//4  Routing
+
+app.get("/hi", function (req, res) {
+  res.end(`<h1 style="background : red">HELLO WORLDddd</h1>`);
+});
+
+app.get("/gift", function (req, res) {
+  res.end(`<h1 style="background : red">Siz sovg'alar bo'limidasiz</h1>`);
+});
+
+
+const server = https.createServer(app);
+let PORT = 3004;
+server.listen(PORT, function () {
+  console.log(`The server is running successfully on port ${PORT}`);
+});
