7d5a8608d4417b4d3be1385374931b8ef0bb4fd8
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-08-20 01:59:56 +0900

    A-TASK

--- train.js
+++ train.js
@@ -1,41 +1,38 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba bo'ling", //0-20
-  "togri boshliq tanlang va koproq xato qiling", //20-30
-  "uzingizga ishlashni boshlang", //30-40
-  "siz kuchli bo'lgan narsalarni qiling", //40-50
-  "yoshlarga investitsiya qiling", //50-60
-  "endi dam oling foydasi yoq", //60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling", //0-20
+//   "togri boshliq tanlang va koproq xato qiling", //20-30
+//   "uzingizga ishlashni boshlang", //30-40
+//   "siz kuchli bo'lgan narsalarni qiling", //40-50
+//   "yoshlarga investitsiya qiling", //50-60
+//   "endi dam oling foydasi yoq", //60
+// ];
 
+//callback
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Insert Number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) return null, list[1];
+//   else if (a > 30 && a <= 40) return null, list[2];
+//   else if (a > 40 && a <= 50) return null, list[3];
+//   else if (a > 50 && a <= 60) return null, list[4];
+//   else {
+//     setInterval(function () {
+//       callback(null , list[5]);
+//     }, 1000);
+//   }
+// }
 
+// console.log('passed 0')
 
-//callback 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("Insert Number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) return null, list[1];
-  else if (a > 30 && a <= 40) return null, list[2];
-  else if (a > 40 && a <= 50) return null, list[3];
-  else if (a > 50 && a <= 60) return null, list[4];
-  else {
-    setInterval(function () {
-      callback(null , list[5]);
-    }, 1000);
-  }
-}
+// maslahatBering(70, (err, data) => {
+//   if(err) console.log('Error:' , err);
+//   else {
+//     console.log(data);
+//   }
+// });
 
-console.log('passed 0')
-
-maslahatBering(70, (err, data) => {
-  if(err) console.log('Error:' , err);
-  else {
-    console.log(data);
-  }
-});
-
-console.log('passed 1')
-
+// console.log('passed 1')
 
 //async functions
 // async function maslahatBering(a) {
@@ -73,8 +70,7 @@ console.log('passed 1')
 
 // console.log("passed here 1");
 
-
-// using await 
+// using await
 
 // async function run() {
 //   let javob = await maslahatBering(6);
@@ -84,3 +80,13 @@ console.log('passed 1')
 // }
 
 // run();
+
+// A-TASK
+
+function countLetters(soz, sanoqHarf) {
+  return soz.split(sanoqHarf).length - 1;
+}
+
+const soz = "Jonibek Sajanim";
+const sanoqHarf = "n";
+console.log(countLetters(soz, sanoqHarf));
