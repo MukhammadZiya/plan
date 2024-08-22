047fcc04b764f90ae8c637969b2e11cd1f1bc891
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-08-22 16:25:22 +0900

    CRUD operatsiyalari

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const res = require("express/lib/response");
 const app = express();
 
 const fs = require("fs");
-const mongodb = require("mongodb");
 
 const db = require("./server").db();
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.log("ERROR:", err);
+//   } else {
+//     user = JSON.parse(data);
+//   }
+// });
 
 //1  Kirish
 app.use(express.static("public"));
@@ -31,6 +30,7 @@ app.set("view engine", "ejs");
 
 //4  Routing
 app.post("/create-item", (req, res) => {
+  console.log("user entered /create-item");
   console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
@@ -43,15 +43,16 @@ app.post("/create-item", (req, res) => {
   });
 });
 
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
+// app.get("/author", (req, res) => {
+//   res.render("author", { user: user });
+// });
+
+// app.get("/", function (req, res) {
+//   res.render("reja");
+// });
 
 app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-app.get("/", function (req, res) {
+  console.log("user entered /");
   db.collection("plans")
     .find()
     .toArray((err, data) => {
@@ -60,7 +61,7 @@ app.get("/", function (req, res) {
         res.end("something went wrong");
       } else {
         console.log(data);
-        res.render("reja");
+        res.render("reja", { items: data });
       }
     });
 });
